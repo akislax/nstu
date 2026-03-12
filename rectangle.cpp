e2e2d54d088856efb4531d432668adf23d06376f
de4dluxe <sas54ru@gmail.com>
2026-03-12 18:07:16 +0700

    Initial commit

---
+++ rectangle.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+static float perimetr (float height, float width) {
+    return 2*(height+width);
+}
+
+static float square (float height, float width) {
+    return height*width;
+}
+
+static double diagonalLenght(float height, float width) {
+    return sqrt(pow(height,2) + pow(width,2));
+}
+
+int main() {
+    float height = 0;
+    cout << "Введите высоту прямоугольника: ";
+    cin >> height;
+    float width = 0;
+    cout << "Введите ширину прямоугольника: ";
+    cin >> width;
+    cout << "Для прямоугольника с высосотой " << height << " и шириной " << width << " параметры равны:" <<  endl;
+    cout << "Периметр: " << perimetr(height, width) << endl;
+    cout << "Площадь: " << square(height, width) << endl;
+    cout << "Длина диагонали: " << diagonalLenght(height, width);
+    return 0;
+}
